4581da3079de98383648f70649db555c0d7266db
Rafael Lima Vieira <rafaelimavieira@gmail.com>
2020-02-24 10:10:41 -0300

    corrigido algoritmo das moedas

--- PAA2018/src/ufc/crateus/dynamicprogramming/Algorithms.java
+++ PAA2018/src/ufc/crateus/dynamicprogramming/Algorithms.java
@@ -2,36 +2,55 @@ package ufc.crateus.dynamicprogramming;
 
 public class Algorithms {
 
-    private int S[][] = new int[1000][1000];
-    private int V[] = new int [1000];
+	private int M[][] = new int[1000][1000];
+	private int S[] = new int[1000];
 
-    public int minCoins(int coins[], int value,int i){
+	public int minCoins(int coins[], int V) {
+		int n = coins.length - 1;
 
-        if(value > 1000) return 0;
-        if(value == 0) return 0;
-        if(value < 0) return Integer.MAX_VALUE/2;
-        if(i == coins.length) return Integer.MAX_VALUE/2;
+		for (int p = 0; p < 1000; p++) {
+			for (int k = 0; k < 1000; k++) {
+				M[p][k] = Integer.MAX_VALUE - 1;
+			}
+		}
 
-        if(S[value][i] == 0){
-             int getCoin = 1 + minCoins(coins,value - coins[i],i);
-             int nextCoin = minCoins(coins,value,i + 1);
+		for (int i = 0; i <= n; i++) // o valor 0 ¢ nao tem representacao, caso base
+			M[0][i] = 0;
 
-             if(getCoin < nextCoin) {
-                 S[value][i] = getCoin;
-                 V[value]= coins[i];
-             }else{
-                 S[value][i] = nextCoin;
-             }
-        }
-        return S[value][i];
-    }
+		for(int v = 1; v <= V; v++) { // testa todo valor v <= V com a primeira moeda do conjunto
+			if (coins[0] == v) {
+				M[v][0] = 1;
+			}
+		} 
 
-   public void printSolutionCoins(int coins[],int value){
-        if(V[value] == 0) return;
+		int a = 0, b = 0;
 
-        System.out.print("["+V[value]+"] ");
-       printSolutionCoins(coins,value - V[value]);
+		for (int v = 1; v <= V; v++) {
 
-   }
+			for (int k = 1; k <= n; k++) {
+
+				b = M[v][k - 1];
+				
+				if ((v - coins[k]) >= 0) {
+					a = 1 + M[v - coins[k]][k - 1];
+				} else {
+					a = Integer.MAX_VALUE - 1;
+				}
+
+				M[v][k] = Math.min(a, b);
+			}
+
+		}
+		return M[V][n] == Integer.MAX_VALUE - 1 ? 0 : M[V][n];
+	}
+
+	public void printSolutionCoins(int coins[], int value) {
+		if (S[value] == 0)
+			return;
+
+		System.out.print("[" + S[value] + "] ");
+		printSolutionCoins(coins, value - S[value]);
+
+	}
 
 }
--- PAA2018/src/ufc/crateus/dynamicprogramming/App.java
+++ PAA2018/src/ufc/crateus/dynamicprogramming/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
 
         Algorithms func = new Algorithms();
-        int coins[] ={10,5,25,50,100,1};
-        System.out.println(func.minCoins(coins,49,0));
-        func.printSolutionCoins(coins,49);
+        int coins[] ={10,1,5,25,1,1,50,100};
+        
+         System.out.println(" 36¢ = " +func.minCoins(coins,36)); //3 moedas
+         System.out.println(" 78¢ = " +func.minCoins(coins,78)); // 5 moedas
+         System.out.println(" 153¢ = " +func.minCoins(coins,153)); // 5 moedas
+         System.out.println(" 179¢ = " +func.minCoins(coins,179)); // sem moedas
+         System.out.println(" 44¢ = " +func.minCoins(coins,44)); // sem moedas
+       
     }
 }
